52001bdae245be8d99c8405521ecb3da57cb22b1
Yi Deng <daviddengcn@gmail.com>
2021-03-26 07:09:47 -0700

    Update ObjectPool.java
    Fix a bug of setting the object in the wrong place in free() method. Refining of comments.

--- ObjectPool.java
+++ ObjectPool.java
@@ -3,11 +3,18 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
 
 /**
  * A lock-free object pool.
+ * This object pool uses AtomicReferenceArray and AtomicInteger but
+ * no blocking locks.
+ *
+ * NOTE The user should not depend on the assumption ObjectPool will
+ * cache all the objects even if the capacity is big enough.
+ * But the situation of objects not cached (with enough capacity)
+ * happens very rare.
  */
 public class ObjectPool<E> implements IObjectFactory<E> {
-	/**
-	 * The underneath object factory.
-	 */
+    /**
+     * The underneath object factory.
+     */
     private final IObjectFactory<E> factory;
     /**
      * The cached object stack.
@@ -32,43 +39,46 @@ public class ObjectPool<E> implements IObjectFactory<E> {
     @Override
     public E alloc() {
         while (true) {
-            // Try reserve a cached object in objects
+            // Try to reserve a cached object in objects
             int n;
             do {
                 n = top.get();
                 if (n == 0) {
-                    // No cached oobjects, allocate a new one
+                    // No cached objects, allocate a new one
                     return factory.alloc();
                 }
             } while (!top.compareAndSet(n, n - 1));
-            // Try fetch the cached object
+            // Try to fetch the cached object
             E e = objects.getAndSet(n, null);
             if (e != null) {
                 return e;
             }
-            // It is possible that the reserved object was extracted before
-            // the current thread tried to get it. Let's start over again.
+            // It is possible that there is no cached object in the reserved
+            // place. E.g. the place was just reserved and not set yet.
+            // Let's simply start over.
         }
     }
     
     @Override
     public void free(E e) {
         while (true) {
-            // Try reserve a place in this.objects for e.
+            // Try to reserve a place in this.objects for e.
             int n;
             do {
                 n = top.get();
                 if (n == objects.length()) {
-                    // the pool is full, e is not cached.
+                    // The pool is full, e is not cached.
                     factory.free(e);
                 }
             } while (!top.compareAndSet(n, n + 1));
-            // Try put e at the reserved place.
-            if (objects.compareAndSet(n + 1, null, e)) {
+            // Try to put e at the reserved place.
+            if (objects.compareAndSet(n, null, e)) {
                 return;
             }
-            // It is possible that the reserved place was occupied before
-            // the current thread tried to put e in it. Let's start over again.
+            // It is possible that the reserved place was occupied. before
+            // the current thread tried to put e in it. E.g. the place was
+            // just reserved in alloc() but not moved out yet
+            // Let's simply start over.
         }
     }
 }
